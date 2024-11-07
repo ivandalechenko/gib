2f868dd941b70cb84b7682cbc654489ed48cad7c
ivandalechenko <ivandalechenko@gmail.com>
2024-11-07 21:46:39 +0200

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.scss'
+
+// const hero = document.getElementById('hero');
+// const blast = document.getElementById('blast');
+// const mission = document.getElementById('mission');
+// const just = document.getElementById('just');
+
+// // Добавляем слушатель для движения мыши
+// document.addEventListener('mousemove', (e) => {
+//   // Получаем позицию курсора как процентное смещение по оси X и Y
+//   const offsetX = (e.clientX / window.innerWidth - 0.5) * 15; // до 10% по горизонтали
+//   const offsetY = (e.clientY / window.innerHeight - 0.5) * 15; // до 10% по вертикали
+
+//   // Применяем смещение к фону
+//   hero.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+//   blast.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+//   mission.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+//   just.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+// });
+
